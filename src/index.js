c5ba424aa3a244a3021547106c5db0c7534cb35f
Matheina-hjk <62123021+Matheina-hjk@users.noreply.github.com>
2020-04-24 14:15:36 +0900

    0424 practice
    compeleted by using code sandbox.. and I don't know why it's not working here

--- src/index.js
+++ src/index.js
@@ -22,3 +22,25 @@ function handleReisze {
 }
 
 window.addEventListener("resize", handleReisze);
+
+const NINE_HOURS_MILLISECONDS = 32400000;
+const clockTitle = document.querySelector("h4");
+
+function getTime() {
+  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const today = new Date();
+  const distance = xmasDay - today;
+  
+  const days = Math.floor(distance / (1000 *60 *60 *24));
+  const hours = Math.floor(distance / (1000*60*60) % 24);
+  const minutes = Math.floor(distance / (1000*60) % 60 );
+  const seconds = Math.floor(distance / 1000 %60);
+
+  clockTitle.innerText = `${days<10 ? `0${days}` : days}d : ${hours  < 10 ? `0${hours }` : hours }h : ${minutes<10 ? `0${minutes}` : minutes}m : ${seconds <10 ? `0${seconds}` : seconds}s`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime,1000);
+}
+init();
